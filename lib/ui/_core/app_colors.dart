486a9bed69862b6dac99766f6be04dbd785d9160
andriele.lange <andriele.lange@aegro.com.br>
2025-04-14 17:57:43 +0000

    first commit

---
+++ lib/ui/_core/app_colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+abstract class AppColors {
+  static const Color backgroundColor = Color(0xFF202123);
+  static const Color mainColor = Color(0xFFFFA559);
+  static const Color pressed = Color(0xFFFF6000);
+  static const Color textHighligth = Color(0xFFFFE6C7);
+  static const Color cardBackground = Color(0xFF343541);
+  static const Color cardText = Color(0xFFDBD7DF);
+}
